1b4e186ba346ae91d4245adc9585d6291594df15
Juliia Kapuza <kapuzajuliia@gmail.com>
2023-03-13 22:09:41 +0200

    tsconfig

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,6 @@
 "use strict";
-let userInput;
-let userName;
-userInput = 5;
-;
-userInput = "Ben";
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError("An error occurred", 500);
+const button1 = document.querySelector('button');
+button1.addEventListener('click', () => {
+    console.log("Clicking");
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
+// два варінти можливі
+const button1 = document.querySelector('button')!
 
+button1.addEventListener('click', () => {
+    console.log("Clicking")
+})
+
+
+
+//  const button1 = document.querySelector('button')
+
+// if (button1) {
+//     button1.addEventListener('click', () => {
+//     console.log("Clicked")
+// })
+// }
